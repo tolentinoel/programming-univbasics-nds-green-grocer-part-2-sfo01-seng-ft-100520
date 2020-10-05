8ac5007fdfe23cfcad9bf462626e347582686839
Ellaine Tolentino <rckrleyn@gmail.com>
2020-10-05 04:34:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,35 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  coupons.each do |coupon|
+    discounted = find_item_by_name_in_collection(coupon[:item], cart)
+    in_cart = !!discounted
+    okay_num = in_cart && discounted[:count] >= coupon[:num]
+    
+    if discounted && okay_num
+      cart << { item: "#{discounted[:item]} W/COUPON", 
+                price: coupon[:cost] / coupon[:num], 
+                clearance: discounted[:clearance],
+                count: coupon[:num]
+              }
+      discounted[:count] -= coupon[:num]
+    end
+    i += 1
+  end
+  cart
 end
 
+  
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |i|
+    if i[:clearance] == true
+      i[:price] *= 0.8
+    
+    end
+    i
+  end
 end
 
 def checkout(cart, coupons)
@@ -22,4 +42,14 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  consolidated = consolidate_cart(cart)
+  final_cart = apply_coupons(consolidated, coupons)
+  apply_clearance(final_cart)
+  
+  to_pay = 0 
+  
+  final_cart.each do |item|
+    to_pay += item[:price] * item[:count]
+  end
 end
